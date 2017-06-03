51f850849de485105835733700847aa32c1f28cc
tesya <tesyant@gmail.com>
2017-06-04 01:50:22 +0700

    dua

---
+++ profilpage.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="dist/semantic.min.css">
+  <link rel="icon" href="img/twh.ico" type="image/x-icon">
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+<script src="dist/semantic.min.js"></script>
+
+  <!-- Standard Meta -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+  <!-- Site Properties -->
+  <title>Prestasi - kusekolah</title>
+
+  <link rel="stylesheet" type="text/css" href="semantic-ui/semantic.css">
+  <link rel="stylesheet" type="text/css" href="homepage.css">
+  <link rel="stylesheet" type="text/css" href="iconfonts/flaticon.css">
+  <link rel="stylesheet" type="text/css" href="dist/components/icon.css">
+  <link rel="stylesheet" type="text/css" href="dist/components/header.css">
+  <link href='/img/twh.ico' rel='shortcut icon'>
+  <link rel="icon" href="img/twh.ico" type="image/x-icon">
+
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.js"></script>
+  <script src="semantic-ui/semantic.js"></script>
+  <script src="homepage.js"></script>
+   <script>
+    $(function(){
+        $('.ui.card').popup();
+    });
+    </script>
+  </head>
+  <body id="home">
+  <div class="ui inverted masthead centered segment">
+    <div class="ui page grid">
+      <div class="column">
+        <div class="ui secondary pointing menu">
+          <a class="logo item">
+            kusekolah
+          </a>
+          <a class="item" href="index.html">
+            <i class="flaticon-home"></i> Beranda
+          </a>
+          <a class="item">
+            <i class="flaticon-graduates"></i> Profil
+          </a>
+          <a class="item">
+            <i class="flaticon-mail"></i> Hubungi Kami
+          </a>
+          <div class="right menu">
+            <div class="item">
+              <div class="ui icon input">
+                <input placeholder="Search..." type="text">
+                <i class="flaticon-position link icon"></i>
+              </div>
+            </div>
+            <a class="ui item" href="login.html">
+              Login
+            </a>
+          </div>
+        </div>
+                   
+        <div class="ui hidden transition information">
+          <h1 class="ui inverted centered header">
+                Prestasi
+          </h1>
+            <p class="ui centered lead">Prestasi-prestasi yang sudah dicapai<br>dari beberapa sekolah.</p>
+          <br>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="ui vertical feature segment">
+    <div class="ui centered page grid">
+      <p>Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet<br>
+      </p>
+    </div>
+  </div>
+    
+  <div class="ui inverted footer segment center">
+    <div class="ui container">
+      <center><img src="img/twh.png" style="width: 40px;height: 40px"></center>
+    </div>
+      <center>
+        <h4 class="ui inverted header">All Right Reserved<br><sub>Copyright 2017</sub></h4>
+      </center>
+  </div>
+</body>
